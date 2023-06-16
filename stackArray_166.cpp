3d5d848ba1b8d9f74edcaf864e41dd3376466abe
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-06-16 11:03:25 +0700

    menambahkan method untuk mengecek data kosong

--- stackArray_166.cpp
+++ stackArray_166.cpp
@@ -11,14 +11,14 @@ private:
 public:
 	StackArray() { //construktor
 		top = -1;
-		
+
 	}
 
 	int push(int element) {
 		if (top == 4) {
 			cout << "Number of data exceeds the limit." << endl;
 			return 0;
-	
+
 		}
 		top++;
 		stack_array[top] = element;	//step 3
@@ -29,7 +29,19 @@ public:
 	}
 
 	void pop() {
+		if (empty()) {//step 1
+			cout << "\nStack is empty. cannot pop." << endl;	//1.a
+			return;	//1b
 
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;	//step 2
+		top--;	//step 3 decrement
+	}
+
+	//method for check if data is empty
+	bool empty() {
+		return (top == -1);
 	}
 
 };
